bc1ad1d5f2d6082292aabc62b9070571bd52120a
Guillermo Veneranda <8316152+gville@users.noreply.github.com>
2021-12-13 00:21:27 -0300

    fix yml spec file path

--- src/swagger-doc.js
+++ src/swagger-doc.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const YAML = require('yamljs');
-var swagger_path =  path.resolve(__dirname,'../doc/weather-forecast-app.yml');
+var swagger_path =  path.resolve(__dirname,'../doc/weather-forecast-api.yml');
 const swaggerDocument = YAML.load(swagger_path);
 
 module.exports = swaggerDocument
